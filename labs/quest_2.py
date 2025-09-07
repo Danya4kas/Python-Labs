d0875ef0b4e61eca9c1d48f2d4daea7cd48631c2
Danya4kas <daniilmosiychuk007@gmail.com>
2025-09-08 00:31:47 +0300

    add quest_2.py

---
+++ labs/quest_2.py
@@ -0,0 +1,23 @@
+num1 =float(input("Введіть перше число:"))
+operation =(input("Введіть операцію: (+, -, *, /):"))
+num2 = float(input("Введіть друге число:"))
+
+if operation == '+':
+    result = num1 + num2
+elif operation == '-':
+    result = num1+ num2
+elif operation == '*':
+    result = num1 * num2
+elif operation == '/':
+    if num2 !=0:
+        result = num1 / num2
+    else:
+        print ("Error: Ділити на 0 неможливо))")
+        exit() 
+else:
+    print("Помилка: Невідома операція!")
+    exit()
+
+print("Результат:", result)
+
+
